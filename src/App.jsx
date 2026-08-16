949ee366c489e1e489099c234f5521c755853c60
Jed Jallorina <jedrikjallorina@gmail.com>
2026-08-16 16:06:36 -0400

    initialize project structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import ncsiLogo from './assets/ncsiLogo.jpg'
+import './App.css'
+
+function App() 
+{
+  return(
+  <>
+    <title>National Civil Stability Index</title>
+    <div class = "header-container">
+      
+    </div>
+  </>
+  )
+}
+
+export default App
